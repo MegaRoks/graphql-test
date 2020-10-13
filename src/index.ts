5cba97a383b565d8feaefdb7becd122c9fbd21a4
Maxim Kamenev <mkamenev@beetsoft.com>
2020-10-13 12:48:19 +0600

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+const port = process.env.SERVER_PORT;
+
+app.get('/', (req: Request, res: Response) => {
+    return res.send('API is working');
+});
+
+app.listen(port, () => {
+    console.log(`Server is listening on ${port}`);
+});
